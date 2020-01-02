c506620fccb75cb9c2e99b8beaea87f53961a022
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-01-02 11:54:51 -0800

    adds more theme

--- src/containers/PlayerInputContainer/PlayerInput.js
+++ src/containers/PlayerInputContainer/PlayerInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ThemeConsumer } from '../../contexts/theme';
 
 class PlayerInput extends React.Component {
     constructor(props) {
@@ -24,29 +25,33 @@ class PlayerInput extends React.Component {
     
     render() {
         return (
-            <form className="column player" onSubmit={ this.handleSubmit }>
-                <label htmlFor='username' className='player-label'>
-                    { this.props.label }
-                </label>
-                <div className='row player-inputs'>
-                    <input
-                        type='text'
-                        id='username'
-                        className='input-light'
-                        placeholder='github username'
-                        autoComplete='off'
-                        value={ this.state.username }
-                        onChange={ this.handleChange }
-                    />
-                    <button
-                        className='btn dark-btn'
-                        type='submit'
-                        disabled={ !this.state.username }
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
+            <ThemeConsumer>
+                {({ theme }) => (
+                    <form className='column player' onSubmit={this.handleSubmit}>
+                        <label htmlFor='username' className='player-label'>
+                            {this.props.label}
+                        </label>
+                        <div className='row player-inputs'>
+                            <input
+                                type='text'
+                                id='username'
+                                className={`input-${theme}`}
+                                placeholder='github username'
+                                autoComplete='off'
+                                value={this.state.username}
+                                onChange={this.handleChange}
+                            />
+                            <button
+                                className={`btn ${theme === 'dark' ? 'light-btn' : 'dark-btn'}`}
+                                type='submit'
+                                disabled={!this.state.username}
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                )}
+            </ThemeConsumer>
         );
     };
 };
